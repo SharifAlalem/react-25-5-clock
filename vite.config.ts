2ce1b528ce252175c0d98e90a46f0f226f084dcc
Sharif Alalem <sharif.alalem@savings-united.com>
2024-11-02 02:04:54 +0100

    add sass & gh-pages & init for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/react-25-5-clock/',
+  css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler', // or "modern", "legacy"
+        }
+      }
+    }
+});
